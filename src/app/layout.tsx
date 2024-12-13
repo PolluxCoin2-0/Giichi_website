e0b5b3a0ea1ff78ae0340550d2e5b440d58a824d
durgeshyadav21 <durgesh@poxscan.io>
2024-12-13 11:05:54 +0530

    code pushed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-    
+      <link rel="icon" href="/Logo.png" type="image/*" />
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
