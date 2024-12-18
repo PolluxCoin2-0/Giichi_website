b3679d595e7369f9c127f392d1beb2568cfb8b3b
durgeshyadav21 <durgesh@poxscan.io>
2024-12-18 15:14:21 +0530

    code pushed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,7 +21,21 @@ export const metadata: Metadata = {
   description: "Giichi IT Solutions is a global leader in Digital Transformation, specializing in Web3, Blockchain, AI, Application Development, UI/UX Design, and Enterprise Solutions. With over 500+ dedicated developers and a presence in Gurgaon, Bangalore, Taiwan, Canada, and the US, we deliver cutting-edge, customer-centric technology services with unmatched quality and timely delivery.",
   viewport: 'width=device-width, initial-scale=1',
   robots: 'index, follow',
-  keywords: "Digital Transformation, Web Development, UI/UX Design, Web3, Blockchain Solutions, AI Development, NFTs, Gaming, DeFi, dApps, Mobile App Development, Enterprise Solutions, CRM, ERP, POS, Digital Strategy, Technology Consulting, Cloud Infrastructure, IoT, AR/VR, Business Intelligence, Clutch Certified IT Company, ISO Certified IT Solutions"
+  keywords: "Digital Transformation, Web Development, UI/UX Design, Web3, Blockchain Solutions, AI Development, NFTs, Gaming, DeFi, dApps, Mobile App Development, Enterprise Solutions, CRM, ERP, POS, Digital Strategy, Technology Consulting, Cloud Infrastructure, IoT, AR/VR, Business Intelligence, Clutch Certified IT Company, ISO Certified IT Solutions.",
+  openGraph: {
+    type: 'website',
+    title: 'Giichi IT Solutions Pvt. Ltd. - Digital Innovation, Web3 & Blockchain, AI, and Application Development.',
+    description: 'Giichi IT Solutions is a global leader in Digital Transformation, specializing in Web3, Blockchain, AI, Application Development, UI/UX Design, and Enterprise Solutions. With over 500+ dedicated developers and a presence in Gurgaon, Bangalore, Taiwan, Canada, and the US, we deliver cutting-edge, customer-centric technology services with unmatched quality and timely delivery.',
+    url: 'https://giichi.network/',
+    images: [
+      {
+        url: '', // Full URL for the image
+        width: 1200,
+        height: 630,
+        alt: 'Digital Innovation, Web3 & Blockchain, AI, and Application Development',
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
